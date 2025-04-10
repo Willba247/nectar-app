7e84eb7fd320a87004be711623c3e468c5a1b38c
galactic-shark <evamichael100@gmail.com>
2025-04-10 23:30:48 +0800

    payment error page + update home svg

--- nectar-app/src/app/payment-error/page.tsx
+++ nectar-app/src/app/payment-error/page.tsx
@@ -4,12 +4,12 @@ export default function PaymentSuccess() {
     return (
         <div className="flex flex-col items-center  min-h-screen bg-black text-white p-4">
             <img
-                src="/payment-success.png"
-                alt="Payment Success"
+                src="/payment-error.png"
+                alt="Payment Error"
                 className="w-full max-w-md mx-auto mb-6"
             />
-            <h1 className="text-3xl font-bold mb-4 text-center">Payment Success!</h1>
-            <p className="text-gray-400 text-center mb-8">Your queue skip has been sent to your inbox.</p>
+            <h1 className="text-3xl font-bold mb-4 text-center">Payment Failed!</h1>
+            <p className="text-gray-400 text-center mb-8">Please try again.</p>
             <Link
                 href="/"
                 className="px-6 py-3 bg-[#0DD2B6] text-white rounded-md hover:bg-[#0DD2B6]/80 transition-colors font-bold flex items-center"
