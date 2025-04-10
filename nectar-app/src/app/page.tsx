834279b792e6b4ace2a99738bcd6b9a51ed34273
galactic-shark <evamichael100@gmail.com>
2025-04-10 23:36:55 +0800

    search bar working

--- nectar-app/src/app/page.tsx
+++ nectar-app/src/app/page.tsx
@@ -1,30 +1,39 @@
-import { HydrateClient } from "@/trpc/server";
+'use client'
 import VenueCard from "./_components/venue-card";
 import { venues } from "@/data/venues";
+import { useState } from 'react';
 
-export default async function Home() {
+export default function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredVenues = venues.filter(venue =>
+    venue.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-black">
-        <div className="flex flex-col gap-4 w-full px-4 items-center">
-          <div className="w-full max-w-md mb-6">
-            <input
-              type="text"
-              placeholder="Search venues..."
-              className="w-full px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-[#0DD2B6]"
-            />
-          </div>
-          {venues.map((venue) => (
-            <VenueCard
-              key={venue.id}
-              name={venue.name}
-              queueSkips={venue.queueSkips}
-              price={venue.price}
-              imageUrl={venue.imageUrl}
-            />
-          ))}
+    <main className="flex min-h-screen flex-col items-center  bg-black">
+      <div className="flex flex-col gap-4 w-full px-4 items-center">
+        <div className="w-full max-w-md mb-6">
+          <input
+            type="text"
+            placeholder="Search venues..."
+            className="w-full px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-[#0DD2B6]"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
         </div>
-      </main>
-    </HydrateClient>
+        {filteredVenues.length > 0 ? filteredVenues.map((venue) => (
+          <VenueCard
+            key={venue.id}
+            name={venue.name}
+            queueSkips={venue.queueSkips}
+            price={venue.price}
+            imageUrl={venue.imageUrl}
+          />
+        )) : (
+          <div className="text-white text-2xl">No venues found</div>
+        )}
+      </div>
+    </main>
   );
 }
